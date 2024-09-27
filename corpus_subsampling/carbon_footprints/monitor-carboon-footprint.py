9f5473b7ff1297c7d452951adebb8eb731fb960b
Maik Fröbe <maik.froebe@uni-jena.de>
2024-09-27 08:51:37 +0200

    mf

---
+++ corpus_subsampling/carbon_footprints/monitor-carboon-footprint.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+tracker = None
+
+try:
+    import contextlib
+    with contextlib.redirect_stdout(None), contextlib.redirect_stderr(None):
+        from time import sleep
+        from codecarbon import OfflineEmissionsTracker
+        tracker = OfflineEmissionsTracker(country_iso_code="DEU", api_call_interval=2, measure_power_secs=1)
+        tracker.start()
+        while True:
+            sleep(20)
+finally:
+    try:
+        if tracker:
+            tracker.stop()
+    except: pass
